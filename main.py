45b896e6926805af8d295161676de52dcbbbe191
kartik07g <72117498+kartik07g@users.noreply.github.com>
2021-08-23 14:02:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'login_info'
+db = MySQL(app)
+# cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+
+
+
+class Inject():
+    def check(self, u, p):
+        self.u = u
+        self.p = p
+        cursor = db.connection.cursor()
+        if cursor is not None:
+            cursor.execute("SELECT * FROM `info` WHERE `username` = '%s' AND `pass` = '%s'" % (u, p))
+            account = cursor.fetchall()
+            print(cursor.rowcount)
+            if account:
+                return 1
+            else:
+                return 0
+        else:
+            return o
+
 
 @app.route('/v1/sanitized/input/', methods=['POST'])
 def result():
     if request.method == 'POST':
         req = request.json
-        input = req["payload"]
-        if input.isalnum():
+        user = req["username"]
+        password = req["password"]
+        c1 = Inject()
+        if c1.check(user, password):
             return jsonify({"result": "Sanitized"})
         else:
             return jsonify({"result": "Unsanitized"})
