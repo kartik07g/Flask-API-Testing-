c42f6f488c2156ee55e0938843686e8e22645094
kartik07g <72117498+kartik07g@users.noreply.github.com>
2021-08-23 14:03:29 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,16 +1,21 @@
 import unittest
-from flask import request, jsonify
-class test_result(unittest.TestCase):
-    def test_result(self):
-        if request.method == 'POST':
-            req = request.json
-            input = req["payload"]
-            if input.isalnum():
-                return jsonify({"result": "Sanitized"})
-            else:
-                return jsonify({"result": "Unsanitized"})
+
+import mysql
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from main import Inject,  app
+
+class test_result(Inject, unittest.TestCase):
+    def test_result1(self):
+
+        user = "kartik07g"
+        password = "k123"
+        c1 = Inject()
+        val = c1.check(user, password)
+
+        self.assertTrue(val)
 
 
 if __name__ == '__main__':
     unittest.main()
-    
